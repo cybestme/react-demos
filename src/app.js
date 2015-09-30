60c306e2fb9f1f40c521b8e981041e725dc7a1c7
cybestme <1804280538@qq.com>
2015-09-30 13:32:46 +0000

    make header

--- src/app.js
+++ src/app.js
@@ -1,20 +1,103 @@
 import React from 'react';
-import Antd from 'antd';
+import antd from 'antd';
+require('./style.css');
 
-var Menu = Antd.Menu;
+var Menu = antd.Menu;
 var SubMenu = Menu.SubMenu;
+var Select = antd.Select;
+var Option = Select.Option;
 
 
-class MyApp extends React.Component {
+var MyApp = React.createClass ({
   render () {
-    return <Menu>
-            <Menu.Item>菜单项</Menu.Item>
-            <SubMenu title="子菜单">
-              <Menu.Item>子菜单项</Menu.Item>
-            </SubMenu>
-          </Menu>
+    return <div>
+            <Header />
+            <Sidebar />
+            <Content />
+            <Footer />
+           </div>
   }
-}
+});
+
+var Header = React.createClass ({
+  getInitialState() {
+    return {
+      current: 'mail'
+    }
+  },
+  handleClick(e) {
+    console.log('click ', e);
+    this.setState({
+      current: e.key
+    });
+  },
+
+  handleChange(value) {
+    console.log('selected ' + value);
+  },
+
+  render() {
+    return <div className="Header clearfix">
+             <div className="logo clearfix">
+               <a><img src="https://t.alipayobjects.com/images/rmsweb/T1B9hfXcdvXXXXXXXX.svg" />
+                 Ant Design
+               </a>
+             </div>
+             <div className="search clearfix">
+              <Select defaultValue="搜索组件..." style={{width:200}} onChange={this.handleChange}>
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+                <Option value="disabled" disabled>Disabled</Option>
+                <Option value="yiminghe">yiminghe</Option>
+                <Option value="jack1">Jack1</Option>
+                <Option value="lucy1">Lucy1</Option>
+                <Option value="disabled" disabled>Disabled</Option>
+                <Option value="yiminghe1">yiminghe1</Option>                 
+              </Select>
+             </div>
+             <div className="nav">
+                  <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal" className="navinside">
+                    <Menu.Item key="mail">
+                      <i className="anticon anticon-mail"></i>导航一
+                    </Menu.Item>
+                    <Menu.Item key="app">
+                      <i className="anticon anticon-appstore"></i>导航二
+                    </Menu.Item>
+                    <SubMenu title={<span><i className="anticon anticon-setting"></i>导航 - 子菜单</span>}>
+                      <Menu.Item key="setting:1">选项1</Menu.Item>
+                      <Menu.Item key="setting:2">选项2</Menu.Item>
+                      <Menu.Item key="setting:3">选项3</Menu.Item>
+                      <Menu.Item key="setting:4">选项4</Menu.Item>
+                    </SubMenu>
+                    <Menu.Item key="alipay">
+                      <a href="http://www.alipay.com/" target="_blank">导航四 - 链接</a>
+                    </Menu.Item>
+                  </Menu>
+             </div>
+         </div>
+  }
+});
+
+var Sidebar = React.createClass ({
+  render() {
+    return <div>
+    </div>
+  }
+});
+
+var Content = React.createClass ({
+  render() {
+    return <div>
+    </div>
+  }
+});
+
+var Footer = React.createClass ({
+  render() {
+    return <div>
+    </div>
+  }
+});
 
 React.render (<MyApp />,
-    document.body)
+    document.body);
